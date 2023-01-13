0b3dbeefa5d495042ca1bee1c1af09cd389c03e0
Ronei Simões <ronei.simoes19@gmail.com>
2023-01-13 17:34:05 -0300

    Finalizando desafio

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,8 +24,39 @@ public class Main {
         mentoria1.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp de java realizado pela DIO");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devRonei = new Dev();
+        devRonei.setNome("Ronei");
+        devRonei.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ronei: " + devRonei.getConteudosInscritos());
+        devRonei.progredir();
+        devRonei.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Ronei: " + devRonei.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Ronei: " + devRonei.getConteudosConcluidos());
+        System.out.println("XP: " + devRonei.calcularTotalXp());
+
+        System.out.println("=========================================================");
+
+        Dev devSimoes = new Dev();
+        devSimoes.setNome("Simões");
+        devSimoes.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Simões: " + devSimoes.getConteudosInscritos());
+        devSimoes.progredir();
+        devSimoes.progredir();
+        devSimoes.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Simões: " + devSimoes.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Simões: " + devSimoes.getConteudosConcluidos());
+        System.out.println("XP: " + devSimoes.calcularTotalXp());
     }
 }
