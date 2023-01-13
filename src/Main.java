ffdc3d2af1b53aceed9d4dbae268877831529786
Ronei Simões <ronei.simoes19@gmail.com>
2023-01-13 15:30:00 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Descrição curso de Python");
+        curso2.setCargaHoraria(5);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de Java");
+        mentoria1.setDescricao("descrição mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
